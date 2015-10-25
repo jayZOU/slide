3f8e27150bfe881cb34f000bc69a20305d349b71
jayzou <417022902@qq.com>
2015-10-25 15:27:27 +0800

    a slide component

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # slide
-a slide component
+滑屏组件
+
+ - 自定义滑屏组件，可定制滑屏动画
+ - 可控制每屏动画效果
+
+##Install
+
+    git clone https://github.com/jayZOU/slide.git
+    npm install
+    gulp
+    
+访问[http://localhost:8080/][1]
+##Examples
+```js
+    /**
+	*	slide 滑屏组件
+	*	@author jayzou
+	*	@time 2015-10-25
+	*	@version 0.0.1
+	*	@class Slide
+	*	@param String	wrap		            必填	传入滑动容器ID
+	*   @param String   currentClass            选填  滑动时切换动画class，默认current
+	*	@param boolean	startLocalstorage		选填	记录当前浏览页面
+	*	@param {Object}	onChange		        选填	切换完成回调
+	*	@param {Object}	onDownChange		    选填	下滑完成时回调
+	*	@param {Object}	onUpChange		        选填	上滑完成时回调
+	*	@param {Object}	defaultClass		    选填	滑动过程动画效果
+	**/
+
+    var slide = new Slide({
+		wrap: 'wrap',					//必填，传入滑动容器ID
+		currentClass: 'current',		//选填，滑动时切换动画class
+		startLocalstorage: false,		//选填，是否开启localstorage记录页面返回后是否回到上次访问的页面，默认false
+		onChange: function(){			//选填，每屏切换完成时的回调
+			console.log("onchange");
+		},
+		onDownChange: function(){		//选填，下滑完成时回调
+			console.log("onDownChange");
+		},
+		onUpChange: function(){		//选填，上滑完成时回调
+			console.log("onUpChange");
+		},
+		defaultClass: {					//选填，滑动过程动画效果
+			'webkitTransition': '-webkit-transform 0.5s ease',	//需要加前缀
+			'transform': 'translate(0px, 0px)'					//不需要加前缀
+		},
+	});
+
+	// slide.next();						//下一页
+	// slide.prev();						//上一页
+	// slide.playTo(3);						//直接跳转第n页
+	// console.log(slide.getPage());		//获取为当前页数
+	// slide.lockPage();					//锁住屏幕，禁止滑动
+	// slide.unLockPage();					//解锁屏幕，允许滑动
+
+	//辅助类
+	// slide.toggleClass(targ, className);	//置换class
+	// slide.addClass(targ, className);		//添加class
+	// slide.removeClass(targ, className);	//删除class
+	// slide.css(o, style);					//添加style样式
+```
+##Notes
+
+ - 滑动容器只能传入ID值，不允许传入class
+
+
+	
+	
+
+
+  [1]: http://localhost:8080/
---
+++ src/js/slide.js
@@ -0,0 +1,225 @@
+var Slide = function(opts) {
+
+	"use strict";
+	//可配置参数
+	var wrap = document.getElementById(opts.wrap) || document.body,
+		currentClass = opts.currentClass || "current", 				//页面动画
+		startLocalstorage = opts.startLocalstorage || false,		//记录currentPage
+		onChange = opts.onChange || function(){},					//每屏切换完成时的回调
+		onDownChange = opts.onDownChange || function(){},			//下滑完成时回调
+		onUpChange = opts.onUpChange || function(){},				//上滑完成时回调
+		defaultClass = opts.defaultClass || {						//页面切换过场动画
+			'transition': '-webkit-transform 0.5s ease',
+			'transform': 'translate(0px, 0px)'
+		},
+
+		//节点相关
+		notes = null, //获取的节点
+		currentPage = 0, //当前页面
+		pageHight = [], //每个页面的高度
+		pageWidth = [], //每个页面的宽度
+
+		//滑屏相关参数
+		x,
+		y,				
+		_x,
+		_y,
+		translateAll = [],			//每屏translate属性
+		isLock = false,				//是否锁屏
+		
+		//检测前缀
+		cssPrefix = '',
+		propPrefix = '',
+		vendors = {'webkit': 'webkit', 'Moz': 'moz', 'ms': 'ms'};
+
+
+	var _init = function() {
+		_initData();
+		_bindEvent();
+	};
+
+	var _initData = function() {
+		//获取每屏的节点
+		notes = wrap.children;
+
+		//获取每屏节点宽高
+		var allHeight = 0;
+		for (var i = 0, len = notes.length; i < len; i++) {
+			notes[i].style.height = notes[i].clientHeight + 'px';
+			notes[i].style.width = notes[i].clientWidth + 'px';
+			pageHight[i] = notes[i].clientHeight;
+			allHeight += notes[i].clientHeight;
+			// console.log(notes[i].style);
+			if(i > 0){
+				translateAll[i] = translateAll[i - 1] + notes[i].clientHeight;
+			}else{
+				translateAll[i] = 0;
+			}
+		}
+		// console.log(translateAll);
+
+		// console.log()
+
+		//处理各平台CSS前缀
+		for(var key in vendors){
+			if (wrap.style[key + "Transform"] !== undefined) {
+				cssPrefix = key + "Transform";
+				propPrefix = key;
+				break;
+			}
+		}
+
+		//初始化滑屏容器style属性
+		var style = objectConcat(defaultClass, {
+			"height": allHeight + 'px',
+			"width": notes[0].clientWidth + 'px'
+		});
+		css(wrap, style);
+
+		//是否记录当前页数
+		if(startLocalstorage && localStorage.page){
+			currentPage = localStorage.page;
+			playTo(currentPage);
+		}
+
+		//默认给第一屏添加动画
+		addClass(notes[0],currentClass);
+	};
+
+	var _bindEvent = function() {
+		wrap.addEventListener('touchstart', function(e) {
+			_touchstart(e);
+		});
+		wrap.addEventListener('touchend', function(e) {
+			_touchend(e);
+		});
+	};
+
+	var _touchstart = function(e) {
+		e.preventDefault();
+		y = e.changedTouches[0].pageY;
+	};
+
+	var _touchend = function(e) {
+		e.preventDefault();
+		_y = e.changedTouches[0].pageY;
+		var disY = _y - y;
+		if(disY < 0){
+			next();
+
+		}else if(disY > 0){
+			prev();
+		}
+	};
+
+	var next = function() {
+		if (currentPage < pageHight.length - 1 && !isLock) {
+			// console.log(translateAll[currentPage + 1]);
+			wrap.style[cssPrefix] = "translate(0px, -" + translateAll[currentPage + 1] + "px)";
+			currentPage ++;
+			localStorage.page = currentPage + 1;
+
+			removeClass(notes[currentPage - 1], currentClass);
+			addClass(notes[currentPage], currentClass);
+
+			//执行回调
+			onChange();
+			onDownChange();
+		}
+
+	};
+
+	var prev = function() {
+		if (currentPage > 0 && !isLock) {
+			wrap.style[cssPrefix] = "translate(0px, -" + translateAll[currentPage - 1] + "px)";
+			currentPage --;
+			localStorage.page = currentPage + 1;
+			removeClass(notes[currentPage + 1], currentClass);
+			addClass(notes[currentPage], currentClass);
+
+			//执行回调
+			onChange();
+			onUpChange();
+		}
+	};
+
+	//直接跳转第n页
+	var playTo = function(page) {
+		wrap.style[cssPrefix] = "translate(0px, -" + translateAll[page - 1] + "px)";
+		currentPage = page - 1;
+
+		//执行回调
+		onChange();
+	};
+
+	//获取为第几屏
+	var getPage = function(){
+		return ++currentPage;
+	};
+
+	//锁住屏幕，禁止滑动
+	var lockPage = function(){
+		isLock = true;
+	};
+
+	//解锁屏幕，允许滑动
+	var unLockPage = function(){
+		isLock = false;
+	};
+
+	var css = function(o, style) {
+		for (var i in style) {
+			o.style[i] = style[i];
+		}
+	};
+
+	var objectConcat = function(o1, o2) {
+		for (var key in o2) {
+			o1[key] = o2[key];
+		}
+		return o1;
+	};
+
+	//toggleClass
+	var toggleClass = function(targ, className) {
+		if (targ.className.indexOf(className) == -1) {
+			//不存在className
+			targ.className = targ.className + ' ' + className;
+		} else {
+			//存在className
+			targ.className = targ.className.replace(" " + className, "");
+		}
+	};
+
+	//添加class
+	var addClass = function(targ, className) {
+		if (targ.className.indexOf(className) == -1) {
+			targ.className = targ.className + ' ' + className;
+		}
+	};
+
+	//删除class
+	var removeClass = function(targ, className) {
+		if (targ.className.indexOf(className) > -1) {
+			targ.className = targ.className.replace(" " + className, "");
+		}
+	};
+
+
+
+
+	_init();
+
+	return {
+		next: next,
+		prev: prev,
+		playTo: playTo,
+		getPage: getPage,
+		lockPage: lockPage,
+		unLockPage: unLockPage,
+		toggleClass: toggleClass,
+		addClass: addClass,
+		removeClass: removeClass,
+		css: css
+	}
+}
